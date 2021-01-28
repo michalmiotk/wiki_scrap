ca54ed87254a20ce4841a9afb70c5aa2cda65df8
mm <michalmiotk95@gmail.com>
2021-01-28 11:37:11 +0100

    yeah itt extract links

--- depr_main.py
+++ depr_main.py
@@ -1,23 +1,8 @@
-import wikipediaapi
 from bs4 import BeautifulSoup
-import re
 
-wiki_html = wikipediaapi.Wikipedia(
-        language='en',
-        extract_format=wikipediaapi.ExtractFormat.HTML
-        )
-ngousa = wiki_html.page("List_of_environmental_and_conservation_organizations_in_the_United_States")
-print(ngousa.links)
-links = []
-def print_sections(sections, level=0):
-    for s in sections:
-        #print("%s: %s - %s" % ("*" * (level + 1), s.title, s.text[0:40]))
-        if s.title not in ['Government agencies', 'See also']:
-            soup = BeautifulSoup(s.text, 'html.parser')
-            print(s.title, " no i gdzie tekst ? ? ? ? ?")
-            print(s.full_text())
-            
-            print("SOUP******************")
-            #soup.prettify()
-        print_sections(s.sections, level + 1)
-print_sections(ngousa.sections)
+from extract_links import extract_links
+
+soup = BeautifulSoup(extract_links(), 'html')
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
--- extract_links.py
+++ extract_links.py
@@ -6,20 +6,22 @@ import numpy as np
 import pandas as pd
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
-url = 'https://en.wikipedia.org/wiki/List_of_environmental_and_conservation_organizations_in_the_United_States'
-html = urlopen(url)
-soup = BeautifulSoup(html, 'xml')
-good_val = ''
-turn_grab=False
-soup_split = str(soup).split("\n")
-joining_on = False
-for s in soup_split:
-    if s.find("Alphabetical listing") > -1:
-        joining_on = True
-    
-    if s.find("References") > -1:
-        joining_on = False
-        print("found refere")
-    if joining_on:
-        good_val.join(' ').join(s)
+def extract_links():
+    url = 'https://en.wikipedia.org/wiki/List_of_environmental_and_conservation_organizations_in_the_United_States'
+    html = urlopen(url)
+    soup = BeautifulSoup(html, 'xml')
+    good_val = ''
+    turn_grab=False
+    soup_split = str(soup).split("\n")
+    joining_on = False
+    for s in soup_split:
+        if s.find("Alphabetical listing") > -1:
+            joining_on = True
+        
+        if s.find("References") > -1:
+            joining_on = False
+            print("found refere")
+        if joining_on:
+            good_val += s
 
+    return good_val
