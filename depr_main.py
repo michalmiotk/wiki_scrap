e92bd98881801a942fbfd486e8293e69092dcf34
m <michalmiotk95@gmail.com>
2021-01-28 11:17:59 +0100

    parse by hand

--- depr_main.py
+++ depr_main.py
@@ -21,30 +21,3 @@ def print_sections(sections, level=0):
             #soup.prettify()
         print_sections(s.sections, level + 1)
 print_sections(ngousa.sections)
-'''
-for i,link in enumerate(links):
-    if key == "Environmental community organizations":
-        print("xxxx")
-    print(value)
-    if i == 2222:
-        wiki_html = wikipediaapi.Wikipedia(
-        language='en',
-        extract_format=wikipediaapi.ExtractFormat.HTML
-        )
-        p_html = wiki_html.page(key)
-        #print(p_html.text)
-        print("SUMARY")
-        (p_html.summary)
-        print("SUMARY")
-        def print_sections(sections, level=0):
-            
-            for s in sections:
-                    #print("%s: %s - %s" % ("*" * (level + 1), s.title, s.text[0:40]))
-                    soup = BeautifulSoup(s.text, 'lxml')
-                    print("SOUP******************")
-                    print(s.title)
-                    print(soup.text)
-                    print("SOUP******************")
-                    print_sections(s.sections, level + 1)
-        print_sections(p_html.sections)
-'''
